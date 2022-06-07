9ff19392aa9c7c9a40a0579cc9fcd8f18895d6e4
iraaz4321 <69959972+iraaz4321@users.noreply.github.com>
2022-06-07 21:43:41 +0300

    changed code to non prototype version

--- main.py
+++ main.py
@@ -13,57 +13,38 @@ import sqlite3
 import time
 import copy
 
-con = sqlite3.connect("starscape_pro.db")
+conn = sqlite3.connect("starscape_pro.db")
 
-cur = con.cursor()
+def color_only_solve(main, colors, connected:int =0):
+    if connected == 0:
+        possible = conn.execute("SELECT id, connected_i, spectral, name, security FROM systems WHERE security='Wild' and spectral=?", [main]).fetchall()
+    else:
+        possible = conn.execute("SELECT id, connected_i, spectral, name, security FROM systems WHERE security='Wild' and spectral=? and connection_count=?", [main, connected]).fetchall()
 
-
-def find_beacon(first_color, second_color, third=None):
-    start = time.time()
-    c = cur.execute("SELECT id, connected_i From systems where spectral=? AND security='Wild' AND connection_count=?", [first_color, len(second_color)]).fetchall()
-
-    possible = []
-
-    for x in c:
-        connected = list(map(int, x[1].split(",")))
-        second_colors = copy.deepcopy(second_color)
-        for connected_system in connected:
-            sql = f"Select spectral from systems where id={connected_system};"
-            color = cur.execute(sql).fetchone()[0]
-            if color in second_colors:
-                second_colors.remove(color)
-            else:
+    possible_s = []
+    for system in possible:
+        connected = conn.execute(f"SELECT id, connected_i, spectral FROM systems WHERE id IN ({system[1]})").fetchall()
+        con = set()
+        col = []
+        level2 = [system[0]]
+        for color in connected:
+            if color[2] not in colors:
                 break
-        else:
-            possible.append(x[0])
-
-    if third is not None:
-        print(possible)
-        for x in possible:
-            third_colors = copy.deepcopy(third)
-            system_col = third_colors.pop(0)
-            sql = f"Select name from systems where id={x} AND connection_count={len(third_colors)} AND spectral='{system_col}';"
-            c = cur.execute(sql).fetchone()
-            if c is None:
-                print(x)
-                possible.remove(x)
             else:
-                print(c[0])
+                level2.append(color[0])
+        for i in connected:
+            col.append(i[2])
+            for x in list(map(int, i[1].split(","))):
+                if x not in level2:
+                    con.add(str(x))
 
-    name_list = []
-    for system_id in possible:
-        sql = f"Select name from systems where id={system_id};"
-        name_list.append(cur.execute(sql).fetchone()[0])
+        connnected_2 = conn.execute(f"SELECT id, connected_i, spectral FROM systems WHERE id IN ({','.join(con)})").fetchall()
+        for color in connnected_2:
+            if color[2] not in colors:
+                break
+            else:
+                col.append(color[2])
+        if sorted(col) == sorted(colors):
+            possible_s.append(system[3])
+    return possible_s
 
-    print(name_list)
-
-
-
-    end = time.time()
-
-    print("Time took: ", end-start)
-
-first_color = "A"
-second_colors = ["B", "B", "G", "G"]
-third_colors = ["B", "A", "G", "G"]
-find_beacon(first_color, second_colors, third_colors)
